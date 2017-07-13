3abfe1e6814af3c33a8ba74ea102af9ca6ff6552
Patrick Lauper <p.lauper@adeon.ch>
2017-07-13 08:42:36 +0200

    Add CompileProjectMojo

--- src/main/java/ch/ivyteam/ivy/maven/CompileProjectMojo.java
+++ src/main/java/ch/ivyteam/ivy/maven/CompileProjectMojo.java
@@ -18,26 +18,13 @@ package ch.ivyteam.ivy.maven;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.repository.RepositorySystem;
 
-import ch.ivyteam.ivy.maven.engine.EngineClassLoaderFactory;
-import ch.ivyteam.ivy.maven.engine.EngineClassLoaderFactory.MavenContext;
 import ch.ivyteam.ivy.maven.engine.MavenProjectBuilderProxy;
 import ch.ivyteam.ivy.maven.util.ClasspathJar;
 import ch.ivyteam.ivy.maven.util.SharedFile;
@@ -49,95 +36,35 @@ import ch.ivyteam.ivy.maven.util.SharedFile;
  * @since 6.0.0
  */
 @Mojo(name=CompileProjectMojo.GOAL, requiresDependencyResolution=ResolutionScope.COMPILE)
-public class CompileProjectMojo extends AbstractEngineMojo
+public class CompileProjectMojo extends AbstractProjectCompileMojo
 {
   public static final String GOAL = "compileProject";
   
-  @Parameter(property = "project", required = true, readonly = true)
-  MavenProject project;
-  
-  /**
-   * Home application where the project to build and its dependencies will be temporary deployed. 
+  /** 
+   * Set to <code>true</code> to bypass the generation of <b>ivy data classes</b>+<b>webservice processes</b> and compilation of <b>java sources</b>.
+   * @since 6.1.0
    */
-  @Parameter(defaultValue = "${project.build.directory}/ivyBuildApp")
-  File buildApplicationDirectory;
-  
-  @Component
-  private RepositorySystem repository;
-  
-  @Parameter(defaultValue="${localRepository}")
-  ArtifactRepository localRepository;
-  
-  static MavenProjectBuilderProxy builder;
-  static File engineAppDirInUse;
-  
+  @Parameter(defaultValue="false", property="ivy.compiler.skip")
+  boolean skipCompilation;
+
   @Override
-  public void execute() throws MojoExecutionException, MojoFailureException
+  protected void compile(MavenProjectBuilderProxy projectBuilder) throws Exception
   {
-    getLog().info("Compiling ivy Project...");
-    compileProject();
-  }
-
-  private void compileProject() throws MojoExecutionException
-  {
-    try
+    if (skipCompilation)
     {
-      getMavenProjectBuilder().compile(project.getBasedir(), resolveIarDependencies());
-      writeDependencyIarJar();
-    }
-    catch (Exception ex)
-    {
-      throw new MojoExecutionException("Failed to compile project '"+project.getBasedir()+"'.", ex);
-    }
-  }
-
-  private MavenProjectBuilderProxy getMavenProjectBuilder() throws Exception
-  {
-    URLClassLoader engineClassloader = getEngineClassloader(); // always instantiate -> write classpath jar!
-    if (builder == null)
-    {
-      builder = new MavenProjectBuilderProxy(engineClassloader, buildApplicationDirectory, getEngineDirectory());
-      engineAppDirInUse = buildApplicationDirectory;
-    }
-    return builder;
-  }
-
-  URLClassLoader getEngineClassloader() throws IOException
-  {
-    MavenContext context = new EngineClassLoaderFactory.MavenContext(
-            repository, localRepository, project, getLog());
-    EngineClassLoaderFactory classLoaderFactory = new EngineClassLoaderFactory(context);
-    return classLoaderFactory.createEngineClassLoader(getEngineDirectory());
-  }
-  
-  private List<File> resolveIarDependencies()
-  {
-    Set<org.apache.maven.artifact.Artifact> dependencies = project.getArtifacts();
-    if (dependencies == null)
-    {
-      return Collections.emptyList();
-    }
-    
-    List<File> dependentIars = new ArrayList<>();
-    for(org.apache.maven.artifact.Artifact artifact : dependencies)
-    {
-      if (artifact.getType().equals("iar"))
-      {
-        dependentIars.add(artifact.getFile());
-      }
-    }
-    return dependentIars;
-  }
-
-  private void writeDependencyIarJar() throws IOException
-  {
-    if (!engineAppDirInUse.isDirectory())
-    { // project has no dependencies
       return;
     }
-    Collection<File> iarJarDepenencies = FileUtils.listFiles(engineAppDirInUse, new String[]{"jar"}, true);
+    
+    getLog().info("Compiling ivy Project...");
+    List<File> iarJars = projectBuilder.createIarJars(getDependencies("iar"));
+    projectBuilder.compile(project.getBasedir(), iarJars, getOptions());
+    writeDependencyIarJar(iarJars);
+  }
+  
+  private void writeDependencyIarJar(Collection<File> iarJarDepenencies) throws IOException
+  {
     if (iarJarDepenencies == null)
-    { // old engine which does not return it's dependencies
+    { // no dependencies
       return;
     }
     File jar = new SharedFile(project).getIarDependencyClasspathJar();
